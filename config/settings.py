243e28788ed5c53abeeb06734a38eab2d9286eca
miyou995 <inter.taki@gmail.com>
2023-04-07 23:57:47 +0100

    new one

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -166,11 +165,11 @@ LOGGING ={
         }
     }
 }
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 MEDIA_ROOT = BASE_DIR / "media"
 
 INTERNAL_IPS = [
